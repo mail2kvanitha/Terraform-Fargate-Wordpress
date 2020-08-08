2756adf2de96c0bdaa72c3ef507a0c0d0bf3fec7
mail2kvanitha <mail2kvanitha@gmail.com>
2020-08-08 20:18:40 +1000

    Security Group File for ALB, ECS and RDS

--- security.tf
+++ security.tf
@@ -41,3 +41,26 @@ resource "aws_security_group" "fargate-sg" {
      cidr_blocks = ["0.0.0.0/0"]
    }
 }
+
+# TRAFFIC FroM ECS TO RDS
+
+resource "aws_security_group" "aurora_sg" {
+  name   = "aurora-sg"
+  description = "Allow ECS Wordpress only to access RDS"
+  vpc_id = aws_vpc.test-vpc.id
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 3306
+    to_port     = 3306
+    security_groups = [aws_security_group.fargate-sg.id]
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
